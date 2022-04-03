3b0f8cfc341551ecd3902839a5ba6aa15234702e
Suxiaohui <41206703@qq.com>
2022-04-03 12:45:10 +0800

    inti

---
+++ pages/about/about.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		onLoad() {
+			console.log("page onload")
+		},
+		onReady: () => {
+			console.log("load connection")
+		},
+		onUnload() {
+			
+		},
+		onHide() {
+			
+		},
+		onShow() {
+			
+		},
+		
+	}
+</script>
+
+<style lang="scss">
+
+</style>
